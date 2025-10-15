7a4c9d27ed1d9c47b4c9a68469baef392c866c51
Judongcha <123702030+Judongchaa@users.noreply.github.com>
2025-10-15 09:40:29 +0200

    Esercizio 1.5.3.e9.c

--- KR-book/1.5.3.e9.c
+++ KR-book/1.5.3.e9.c
@@ -2,20 +2,26 @@
 
 int main() {
 	int char_counter = 0;
-	int blank_counter = 0;
+    int prev_space = 0;
 	int c;
 
-	char phrase[] = {};
+	char phrase[10] = {0};
 
 	while ((c = getchar()) != 10) {
 		if (c != 32) {
+            prev_space = 0;
 			phrase[char_counter] = c;
-			char_counter++;	
+            char_counter++;
 		} else {
-			phrase[char_counter];
-		}
+			phrase[char_counter] = c;
+            if (!prev_space) char_counter++;
+            prev_space = 1; 
+                
+        }
 	}
-	for(int i; i<char_counter; i++) { 
-		printf("%c", phrase[i]);
-		}
+	for(int i = 0; i<char_counter; i++) { 
+		printf("%c", phrase[i]);	
+    } 
+    printf("\n");
+
 }
