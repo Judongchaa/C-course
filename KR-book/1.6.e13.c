56f7488eb77d5df6cc8db1323ba768078c14da01
Judongcha <carlo.mazzaraa@gmail.com>
2025-10-17 11:48:23 +0200

    Esercizio

--- KR-book/1.6.e13.c
+++ KR-book/1.6.e13.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
 /* count digits, white space, others */
 int main() {
-	int c, i, nwhite, nother;
-	int ndigit[10];
+	int c, i, char_per_word;
+	int nwords[10];
 
-	nwhite = nother = 0;
+	int state = OUT;
+
+	char_per_word = 0;
 	for (i = 0; i < 10; ++i)
-		ndigit[i] = 0;
+		nwords[i] = 0;
 	
+	i = 0;
+
 	while ((c = getchar()) != EOF)
-		if (c >= '0' && c <= '9')
-			++ndigit[c-'0'];
-		else if (c == ' ' || c == '\n' || c == '\t')
-			++nwhite;
-		else
-			++nother;
+		if (c == ' ' || c == '\n' || c == '\t') {
+			if (state == IN) {
+				state = OUT;
+				nwords[i] = char_per_word;
+				char_per_word = 0;
+				i++;
+			}			
+		} else {
+			state = IN;
+			char_per_word++;
+		}
 		
-		printf("digits =");
-		for (i = 0; i < 10; ++i)
-			printf(" %d", ndigit[i]);
-		printf(", white space = %d, other = %d\n", nwhite, nother);
+		printf("Histogram: \n");
+		for (int j = 0; j < i; j++) {
+			printf("%d:", j);
+			while (nwords[j]) {
+				printf("*");
+				nwords[j]--;
+			}
+			printf("\n");
+		}
 }
