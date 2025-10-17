5a4cb9c31e740e61ae4bf0a757caee23099a7748
Judongcha <carlo.mazzaraa@gmail.com>
2025-10-17 18:13:26 +0200

    1.9

---
+++ KR-book/1.8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int power(int m, int n);
+
+/* test power function */
+int main() {
+	int i;
+	for (i = 0; i < 10; ++i)
+		printf("%d %d %d\n", i, power(2,i), power(-3,i));
+		return 0;
+}
+
+/*
+ * power: raise base to n-th power; n >= 0 
+ *
+ * tryng to use less variables to take advantage of local
+ * variables in C. Modifyng the n variable doesn-t affect the 
+ * value of n in the main function
+ *
+ */
+int power(int base, int n) {
+	while (n) {
+		base = base * base;
+		n--;
+	}
+	return base;
+}
