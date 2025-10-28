e20d88018c3b67298d3f469ff14aec5912de648f
Judongcha <123702030+Judongchaa@users.noreply.github.com>
2025-10-28 14:00:03 +0100

    Esercizio 2.2.e1.c finito

--- KR-book/2.2.e1.c
+++ KR-book/2.2.e1.c
@@ -1,36 +1,48 @@
 #include <stdio.h>
 #include <limits.h>
+#include <float.h>
+#include <math.h>
 
-#define STEP 10000
+#define STEP 2
 #define PRECISION 0.01
 
 void int_limits(int num, int step);
+void float_limits(float num,float step);
 
 int main(void) {
     int intNum = 0;
     signed int sIntNum = 0;
-
-    printf("%d\n", INT_MAX);
-
-    max_int = int_limits(intNum, STEP);
-    min_int = int_limits(sIntNUm, -STEP);
+    
+    float floatNum = 1;
+    printf("FLT_MAX       = %e\n", FLT_MAX);
+    float_limits(floatNum, STEP);
 }
 
 void int_limits(int num, int step) {
 
     int oldNum = num;
     while(step >= 1) {
-	if(num+step > oldNum) {
-		num += step;
-		oldNum = num;
-	} else {
-		step--;
-	}
-	        
+    	if(num+step > oldNum) {
+	    	num += step;
+		    oldNum = num;
+    	} else {
+	    	step--;
+	    }    
     }
 
     printf("Maximum value of int: %d\n", num);
 
 }
 
-
+void float_limits(float num, float step) {   
+    while(step >= 1) {
+        
+    	if(isinf(num*step)) {
+	    	step -= 0.0000001;
+            continue;
+        }
+    	num *= step;
+        printf("%e\n",num);
+    }
+    printf("Maximum value of float: %e\n", num);
+}
