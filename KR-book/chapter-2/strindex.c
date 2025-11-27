fb6e928e2b1211e554c88511d2e67fe4d3b484f4
Judongcha <carlo.mazzaraa@gmail.com>
2025-11-27 14:30:53 +0100

    commit

--- KR-book/chapter-2/strindex.c
+++ KR-book/chapter-2/strindex.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+int strindex(char s[], char t[]);
+
+int main(void)
 {
-  return EXIT_SUCCESS;
+  char s[] = "Hello world or hello bassano\n";
+  char t[] = "or";
+
+  int pos = strindex(s, t); 
+  printf("%d\n", pos);
+
+  return 0;
 } 
+
+int strindex(char s[], char t[])
+{
+  int rightmost = -1;
+  int slen = strlen(s);
+  int tlen = strlen(t);
+
+  for(int i = 0; i<slen; i++)
+  {
+    if(s[i] == t[0]) {
+      int matching = 1;
+
+      for (int j = 1; j<tlen; j++) 
+      {
+        i++;
+        matching++;
+        if (matching == tlen) rightmost = (i-tlen);
+      }
+    }
+ 
+  }
+  return rightmost;
+}
