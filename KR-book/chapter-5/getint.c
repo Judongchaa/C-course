f3a07d1244f53f0c9847341cec6af166885bd373
Judongcha <carlo.mazzaraa@gmail.com>
2025-12-15 12:55:42 +0100

    hhjhj

--- KR-book/chapter-5/getint.c
+++ KR-book/chapter-5/getint.c
@@ -2,29 +2,22 @@
 #include <stdio.h>
 
 #define SIZE 20
+#define iprint(expr) printf(#expr " = %d\n", expr)
 
 int getint(int *pn);
-int getch(void);
-void ungetch(int);
 
-int main (void) {
+int main(void) {
     int n, i;
     int array[SIZE] = {0};
 
     printf("Enter input (e.g., '123 45 -6'):\n");
 
     // The input loop
-    for (n = 0; n < SIZE;) {
-        getint(&array[n]);
-        int c = array[n];
-        if (c != 0)
-            n++;
-        if (c == '\n')
-            break;
-    }
+    for (n = 0; n < SIZE && getint(&array[n]); n++)
+        ;
 
     printf("\nParsed Integers:\n");
-    for (i = 0; i <= n; i++) {
+    for (i = 0; i < n; i++) {
         printf("%d ", array[i]);
     }
     printf("\n");
@@ -32,54 +25,28 @@ int main (void) {
     return 0;
 }
 
+
 /* getint: get next integer from input into *pn */
 int getint(int *pn) {
-  int c, sign;
+    int c, sign;
+    while (isspace(c = getchar())) /* skip white space */
+        ;
 
-  while (isspace(c = getch())); /* skip white space */
-
-  if (c == '\n'){
-      return '\n';
-  }
-
-  if (!isdigit(c) && c != '+' && c != '-') {
-    ungetch(c); /* it is not a number */
-    return 0;
-  }
-
-  sign = (c == '-') ? -1 : 1;
-  if (c == '+' || c == '-') {
-    c = getch();
-  }
-
-  for (*pn = 0; isdigit(c); c = getch()) {
-    *pn = 10 * *pn + (c - '0');
-  }
-
-  *pn *= sign;
-  if (c != '\n') ungetch(c);
-  return c;
-}
-
-
-int buf = 0;
-
-int getch(void) {
-    int temp;
-
-    if (buf != 0) {
-        int temp = buf;
-        buf = 0;
-        return temp;
-    } else
-        return getchar();
-}
-
-void ungetch(int c) {
-    if (buf != 0) {
-        printf("Error: Buffer overflow");
-        return;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetc(c, stdin); /* it is not a number */
+        return 0;
     }
 
-    buf = c;
+    sign = (c == '-') ? -1 : 1;
+
+    if (c == '+' || c == '-')
+        c = getchar();
+
+    for (*pn = 0; isdigit(c); c = getchar())
+        *pn = 10 * *pn + (c - '0');
+
+    *pn *= sign;
+    if (c != EOF)
+        ungetc(c, stdin);
+    return c;
 }
