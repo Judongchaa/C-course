6098a72ad75442af5336f85cdd9032b0da472701
Judongchaa <carlo.mazzaraa@gmail.com>
2026-01-04 15:27:43 +0100

    stack system finalized

--- Sanfilippo/toyforth/toyforth.c
+++ Sanfilippo/toyforth/toyforth.c
@@ -1,4 +1,7 @@
+#include <alloca.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define TFOBJ_TYPE_NUM 0
@@ -6,6 +9,7 @@
 #define TFOBJ_TYPE_STR 2
 #define TFOBJ_TYPE_LIST 3
 #define TFOBJ_STR_MAXLEN 10
+
 
 /* ============================================= Data Structure ========================================================== */
 
@@ -22,7 +26,7 @@ typedef struct tfobj{
         } str;
 
         struct {  // TFOBJ_TYPE_LIST
-            struct tfobj *stack;
+            struct tfobj *stack[3];
             int len;
         } list;
     };
@@ -32,102 +36,117 @@ typedef struct tfobj{
 * It is an array of tfobj, which can be num, op, str or list.
 *
 * The TFOBJ_TYPE_LIST object has the same structure of a tfstack object.
-* the stack is the main container, which can contain variius tfobj that can also act
+* the stack is the main container, which can contain variius tfobj pointers that can also act
 * as a little substacks. */
 typedef struct tfstack {
-    tfobj stack[50];
+    tfobj *stack[50];
     int len;
 } tfstack;
 
+/* ========================================== Functions declaration ==================================================== */
+
+void *xmalloc(size_t size);
+void objremove(tfobj *o);
 
 /* ============================================== Stack functions ====================================================== */
 
-/* creates a new global stack obj with len 0 */
+/* creates a new stack obj in the heap sugin malloc, with len 0 */
 tfstack* stack_create(void) {
-    static tfstack o;
-    o.len = 0;
+    tfstack *o = xmalloc(sizeof(tfstack));
+    o -> len = 0;
 
-    return &o;
+    return o;
 }
 
-/* appends to the last position the tfobj pointed at by tfstack */
+/* appends to the last position the tfobj pointer to tfstack */
 void stack_append(tfobj *object, tfstack *st) {
     int len = st->len;
-    st->stack[len] = *object;
+    st->stack[len] = object;
     (st->len)++;
 }
 
-/* pops the tfobj at the last position of the tfstack */
+/* pops the tfobj at the last position of the tfstack, freeing the heap */
 void stack_pop(tfstack *st) {
-    (st->len)--;
+    int len = --(st->len);
+    tfobj *o = st->stack[len];
+    objremove(o); //check the type of the tfobj and acts differently depending on it
 }
 
 /* recursive function to print every tfobj inside the stack.
-* if it finds a TFOBJ_TYPE_LIST it parse it as a tfstack and recall the
-* stack_print function */
+ * if it finds a TFOBJ_TYPE_LIST it parse it as a tfstack and recall the
+ * stack_print function                                                  */
 void stack_print(tfstack *st) {
     int len = st->len;
-    tfobj *o = &(st->stack[0]);
+    tfobj *o = (st->stack[0]);
 
-    printf("[ ");
-    for(int j = 0; j<len; j++, o++) {
+    printf(" [");
+    for(int j = 0; j<len; j++, o=(st->stack[j])) {
         if(o->type == TFOBJ_TYPE_NUM)
-            printf ("%d ", o->num);
+            printf (" %d", o->num);
         else if (o->type == TFOBJ_TYPE_OP)
-            printf ("%c ", o->op);
+            printf (" %c", o->op);
         else if (o->type == TFOBJ_TYPE_STR)
-            printf ("%s ", o->str.symbol);
+            printf (" %s", o->str.symbol);
         else
             stack_print((tfstack *) &o->list);
     }
-    printf("]");
+    printf(" ]");
 }
 
 
-/* ======================================== Data structures functions ================================================ */
+/* ======================================== tfobj manipulation functions ================================================ */
 
-
+/* creates a new tfobj in the heap using malloc, with refcount 1 and the passed type */
 tfobj* objdefine(int type) {
-    static tfobj o;
-    o.type = type;
-    o.refcount = 1;
+    tfobj *o = xmalloc(sizeof(tfobj));
+    o->type = type;
+    o->refcount = 1;
 
-    return &o;
+    return o;
 }
 
+/* Duplicate the passed pointer, increase the obj refcount and adds the pointer to the last position of the stack */
 void objdup(tfstack *st) {
     int last = st->len -1;
     if (last>=0) {
-        (st->stack[last].refcount)++;
-        stack_append(&st->stack[last], st);
+        (st->stack[last]->refcount)++;
+        stack_append(st->stack[last], st);
     }
 }
 
-void objnum (int num, tfstack *st) {
+tfobj* objnum (int num, tfstack *st) {
     tfobj* o = objdefine(TFOBJ_TYPE_NUM);
     o -> num = num;
 
     stack_append(o, st);
+
+    return o;
 }
 
-void objop (char op, tfstack *st) {
+tfobj* objop (char op, tfstack *st) {
     tfobj* o = objdefine(TFOBJ_TYPE_OP);
     o -> op = op;
 
     stack_append(o, st);
+
+    return o;
 }
 
 
-void objsym (char *symbol, tfstack *st) {
+tfobj* objsym (char *symbol, tfstack *st) {
     tfobj* o = objdefine(TFOBJ_TYPE_STR);
     o -> str.symbol = symbol;
     o -> str.len = strlen(symbol);
 
     stack_append(o, st);
+
+    return 0;
 }
 
-void objvar (char *var, tfstack *st) {
+tfobj* objvar (char *var, tfstack *st) {
     objsym(var, st);
+
+    return 0;
 }
 
 tfobj* objlist (void) {
@@ -135,45 +154,67 @@ tfobj* objlist (void) {
     return o;
 }
 
-void tfprint(tfobj *o) {
-    if(o->type == TFOBJ_TYPE_NUM) {
-        printf ("The obj is the number: %d\n", o->num);
-    } else if (o->type != TFOBJ_TYPE_LIST) {
-        printf ("The obj is the symbol: %s\n", o->str.symbol);
+void objremove(tfobj *o) {
+    short type = o->type;
+
+    if(type == TFOBJ_TYPE_NUM ||type == TFOBJ_TYPE_OP) {
+        free(o);
+    }
+    else if (o->type == TFOBJ_TYPE_STR) {
+        free(o->str.symbol);
+        free(o);
+    }
+    else {
+        for(int i=0; i < o->list.len; i++)
+            objremove(o->list.stack[i]);
+        free(o);
     }
 }
+
+void tfprint(tfobj *o) {
+    if(o->type == TFOBJ_TYPE_NUM)
+        printf (" %d", o->num);
+    else if (o->type == TFOBJ_TYPE_OP)
+        printf (" %c", o->op);
+    else if (o->type == TFOBJ_TYPE_STR)
+        printf (" %s", o->str.symbol);
+    else
+        stack_print((tfstack *) &o->list);
+}
+
+/* =============================================== Memory rappers ====================================================== */
+void *xmalloc(size_t size) {
+    void *ptr = malloc(size);
+    if(ptr == NULL) {
+        fprintf(stderr, "Not enought memory");
+        free(ptr);
+    }
+
+    return ptr;
+}
+
+/* ==================================================== Getop() =========================================================== */
+
+void getop(FILE *input, tfstack *stack) {}
 
 /* ==================================================== MAIN =========================================================== */
 
 int main(int argc, char **argv) {
-    // if (argc == 1 || argc > 3) {
-    //     fprintf(stderr, "Correct usage: toyforth <cli/open>");
-    // }
-    // else if (!strcmp(argv[1], "open") ) {
-    //     FILE *readfrom = fopen(argv[2], "r");
-    //     printf("open");
-    //     if (readfrom != NULL) {
-    //         // get_element(readfrom);
-    //     } else {
-    //         fprintf(stderr, "File <%s> cannot be opened", argv[2]);
-    //     }
-    // }
-    // else if (!strcmp(argv[1], "cli")) {
-    //     //get_element(stdin);
-    //     printf("cli");
-    // } else {
-    //     fprintf(stderr, "Correct usage: toyforth <cli/open>");
-    // }
+    if (argc == 1 || argc > 3) {
+        fprintf(stderr, "Correct usage: toyforth <cli/open>\n");
+    }
+    else if (!strcmp(argv[1], "open") ) {
+        FILE *readfrom = fopen(argv[2], "r");
+        if (readfrom != NULL) {
+            // get_element(readfrom);
+        } else {
+            fprintf(stderr, "File <%s> cannot be opened\n", argv[2]);
+        }
+    }
+    else if (!strcmp(argv[1], "cli")) {
+        //get_element(stdin);
+    } else {
+        fprintf(stderr, "Correct usage: toyforth <cli/open>");
+    }
 
-    tfstack *stack = stack_create();
-    objnum(10, stack);
-    objsym("ciao", stack);
-    objdup(stack);
-    objop('f', stack);
-    tfobj *list1 = objlist();
-    objnum(80, (tfstack *) &list1->list);
-    stack_append(list1, stack);
-
-    stack_print(stack);
-    printf("\n");
 }
